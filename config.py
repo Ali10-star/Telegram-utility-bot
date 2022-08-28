763a4d3f9ef2b901ec3609a94f06add0692fbe46
Ali Arous <Alosh981@hotmail.com>
2022-08-28 13:45:54 +0300

    Final webhooks try

--- config.py
+++ config.py
@@ -3,11 +3,14 @@ from telegram import Bot
 from asyncio import Queue
 
 class BotConfig:
-    def __init__(self, token: str) -> None:
-        # .token("TOKEN")
-        self.application = Application.builder() \
-                            .updater(Updater(bot=Bot(token), update_queue=Queue)) \
-                            .build()
+    def __init__(self, token: str, with_updater: bool = True) -> None:
+        if with_updater:
+            self.application = Application.builder() \
+                                .updater(Updater(bot=Bot(token), update_queue=Queue)) \
+                                .build()
+        else:
+            self.application = Application.builder().token(token).build()
+
 
     def get_application(self) -> Application:
         return self.application
--- main.py
+++ main.py
@@ -15,13 +15,16 @@ PORT = int(5000)
 ENV = os.getenv('ENV', 'DEV')
 
 if __name__ == '__main__':
-    bot_config = BotConfig(API_KEY)
-    bot_config.register_handlers(handlers_list)
-    application = bot_config.get_application()
     # run(application.bot.set_webhook(url=f"{WEB_HOOK}/{API_KEY}"))
     if ENV == 'DEV':
+        bot_config = BotConfig(API_KEY, with_updater=False)
+        bot_config.register_handlers(handlers_list)
+        print("In dev mode (;")
         bot_config.run()
     else:
+        bot_config = BotConfig(API_KEY)
+        bot_config.register_handlers(handlers_list)
+        application = bot_config.get_application()
         run(application.run_webhook(listen="0.0.0.0", port=PORT, url_path=API_KEY,
                                     webhook_url=f"{WEB_HOOK}/{API_KEY}"))
 
